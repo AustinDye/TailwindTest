3d90642307a6df6b9b38417effd7c38afa73ed9e
Austin Dye <101369015+AustinDye@users.noreply.github.com>
2023-01-29 02:08:18 -0800

    some fun

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -9,13 +9,20 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold">
-    Hello world!
-  </h1>
+  <div class="bg-white py-8 px-8 max-w-sm mx-auto  rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+    <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="https://thiscatdoesnotexist.com/" alt="">
+    <div class=" text-center space-y-2 sm:text-left text-black">
+      <div class="space-y-0.5">
+        <p class=" text-lg text-black font-semibold">Gary Cat</p>
+        <p class="text-slate-500 font-medium">Cool Guy</p>
+      </div>
+      <button class="bg-white px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+      
+    </div>
+   
+  </div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
