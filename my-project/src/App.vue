84571837de9bdbf3406ac9acb1fce63aefec506e
Austin Dye <101369015+AustinDye@users.noreply.github.com>
2023-01-29 16:38:20 -0800

    layut

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -3,10 +3,15 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  
-  <HelloWorld msg="Vite + Vue" />
+  <body class=" mx-20">
+    <HelloWorld msg="Vite + Vue" />
+  </body>
+
 </template>
 
 <style scoped>
-
+body{
+  background-color: white;
+  box-shadow: 0 0 .5em .5em white;
+}
 </style>
--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ const count = ref(0)
 </script>
 
 <template>
-  <div class="bg-white py-8 px-8 max-w-sm mx-auto  rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+  <!--<div class="  bg-white py-8 px-8 max-w-sm mx-auto  rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
     <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="https://thiscatdoesnotexist.com/" alt="">
     <div class=" text-center space-y-2 sm:text-left text-black">
       <div class="space-y-0.5">
@@ -20,7 +20,97 @@ const count = ref(0)
       
     </div>
    
+  </div> -->
+
+  <div class="text-7xl font-bold text-center text-white items-center justify-center block gap-x-10 bg-dark mt-28 bg-black lg:flex lg:text-9xl lg:h-96">
+    <div class="py-9 lg:p-0">
+      <h1>Austin Dye</h1>
+      <p class="text-xl my-5  text-slate-400"> Deveoper, Desiger, Detail Oriented </p>
+    </div>
+
+    <img src="https://avatars.githubusercontent.com/u/101369015?v=4" alt="">
   </div>
+  <div class="container mx-auto  mt-72">
+    <div class="flex items-center justify-center">
+      <div class="bg-black w-full p-24">
+      </div>
+      <div class="text-black w-full p-20 ">
+        <h2 class="text-6xl font-bold my-5">Full-Stack Developer</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat id, magni dignissimos voluptates excepturi dolorem ratione nesciunt quidem sit. Dolor rem mollitia repellat architecto sit explicabo quidem, consequuntur ad aliquid?</p>
+        <div class="columns-2">
+          
+        </div>
+      </div>
+     
+    </div>
+  </div>
+
+  <div class="container flex justify-center mx-auto">
+    <div class="columns-4 gap-24">
+    
+      <div class=" w-48 ">
+        <img class=" " src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/2560px-Node.js_logo.svg.png" alt="">
+      </div>
+
+      <div class=" w-48">
+        <img class="" src="https://logovectorseek.com/wp-content/uploads/2019/10/bootstrap-logo-vector.png" alt="">
+      </div>
+      <div class="w-48 pt-10">
+        <img class="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Tailwind_CSS_logo.svg/2560px-Tailwind_CSS_logo.svg.png" alt="">
+      </div>
+      <div class=" w-48">
+        <img class="" src="https://www.vectorlogo.zone/logos/vuejs/vuejs-ar21.png" alt="">
+      </div>
+    
+    
+    </div>
+    
+  </div>
+
+  <div class="container mx-auto mt-32">
+    <div class="flex items-center">
+      
+      <div class="text-black w-full p-20 ">
+        <h2 class="text-6xl font-bold my-5">QA Test Automation</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat id, magni dignissimos voluptates excepturi dolorem ratione nesciunt quidem sit. Dolor rem mollitia repellat architecto sit explicabo quidem, consequuntur ad aliquid?</p>
+        <div class="columns-2">
+          
+        </div>
+      </div>
+      <div class="bg-black w-full p-24">
+      </div>
+     
+    </div>
+  </div>
+
+  <div class="container flex justify-center mx-auto">
+    <div class="columns-4 gap-24">
+    
+      <div class="h-52 w-48 ">
+        <img class=" " src="https://www.testgrid.io/blog/wp-content/uploads/2021/05/appium.png" alt="">
+      </div>
+
+      <div class=" h-72 w-48">
+        <img class="" src="https://seeklogo.com/images/W/webdriverio-logo-7E0F1B52E5-seeklogo.com.png" alt="">
+      </div>
+      <div class="h-72 w-48">
+        <img class="" src="https://camo.githubusercontent.com/58045a79a69afea4cab1cea6def6d911fba3956cf5fd683addf41c032aa64088/68747470733a2f2f636c6475702e636f6d2f78465646784f696f41552e737667" alt="">
+      </div>
+      <div class="h-72 w-48 pt-10">
+        <img class="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Selenium_logo.svg/2560px-Selenium_logo.svg.png" alt="">
+      </div>
+    
+    
+    </div>
+    
+  </div>
+
+  
+ 
+
+    
+
+ 
 </template>
 
 <style scoped>
