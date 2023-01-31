dd5c821b270c72fe6863545ee56b3aa607b2948e
Austin Dye <101369015+AustinDye@users.noreply.github.com>
2023-01-31 13:17:03 -0800

    projects-section

--- my-project/src/components/HelloWorld.vue
+++ my-project/src/components/HelloWorld.vue
@@ -28,21 +28,21 @@ const count = ref(0)
         <h1>Austin Dye</h1>
         <p class="text-xl my-5 text-slate-400"> Developer, Desiger, Detail Oriented </p>
       </div>
-      <div class="bg-green h-1/2 p-10 border-solid border-2 ">
-        <img class=" bg-pink rounded-full border-solid border-2" src="../assets/img/me.png" alt="">
+      <div class=" h-1/2 p-10 border-solid border-2 ">
+        <img class=" bg-white rounded-full border-solid border-2" src="../assets/img/me.png" alt="">
       </div>
    
 
   </div>
 
   <div class="border-solid border-2 text-black mt-72 p-10">
-    <div class="container mx-auto bg-black  ">
+    <div class="container mx-auto">
       <div class="flex items-center justify-center">
       
-        <div class=" text-black bg-blue w-1/2 p-20 border-solid border-2">
-          <h2 class="text-7xl text-white font-bold">Full-Stack Developer</h2>
+        <div class=" text-black  w-1/2 p-20 border-solid border-2">
+          <h2 class="text-7xl  font-bold">Full-Stack Developer</h2>
         </div>
-        <div class="text-white bold w-1/2 px-10 ">
+        <div class=" bold w-1/2 px-10 ">
           <p class="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat id, magni dignissimos voluptates excepturi dolorem ratione nesciunt quidem sit. Dolor rem mollitia repellat architecto sit explicabo quidem, consequuntur ad aliquid?</p>
           <div class="columns-2">
             
@@ -74,43 +74,65 @@ const count = ref(0)
     </div>
   
   </div>
- 
-  <div class="container mx-auto mt-32">
-    <div class="flex items-center">
-      
-      <div class="text-black w-full p-20 ">
-        <h2 class="text-6xl font-bold my-5">QA Test Automation</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat id, magni dignissimos voluptates excepturi dolorem ratione nesciunt quidem sit. Dolor rem mollitia repellat architecto sit explicabo quidem, consequuntur ad aliquid?</p>
+  <div class="border-solid border-2 text-black mt-24 mb-24 p-10">
+  <div class="container mx-auto">
+    <div class="flex items-center justify-center">
+    
+     
+      <div class=" bold w-1/2 px-10 ">
+        <p class="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat id, magni dignissimos voluptates excepturi dolorem ratione nesciunt quidem sit. Dolor rem mollitia repellat architecto sit explicabo quidem, consequuntur ad aliquid?</p>
         <div class="columns-2">
           
         </div>
       </div>
-      <div class="bg-black w-full p-24">
+      <div class=" text-black  w-1/2 p-20 border-solid border-2">
+        <h2 class="text-7xl  font-bold">QA Test Automation</h2>
       </div>
      
     </div>
   </div>
 
   <div class="container flex justify-center mx-auto">
-    <div class="columns-4 gap-24">
+    <div class="columns-4 mt-10 gap-24">
     
-      <div class="h-52 w-48 ">
+      <div class="w-48 p-2">
         <img class=" " src="https://www.testgrid.io/blog/wp-content/uploads/2021/05/appium.png" alt="">
       </div>
 
-      <div class=" h-72 w-48">
+      <div class="w-48 p-2">
         <img class="" src="https://seeklogo.com/images/W/webdriverio-logo-7E0F1B52E5-seeklogo.com.png" alt="">
       </div>
-      <div class="h-72 w-48">
+      <div class="w-48 p-6 pt-0">
         <img class="" src="https://camo.githubusercontent.com/58045a79a69afea4cab1cea6def6d911fba3956cf5fd683addf41c032aa64088/68747470733a2f2f636c6475702e636f6d2f78465646784f696f41552e737667" alt="">
       </div>
-      <div class="h-72 w-48 pt-10">
+      <div class="w-48 p-2 pt-10">
         <img class="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Selenium_logo.svg/2560px-Selenium_logo.svg.png" alt="">
       </div>
     
     
     </div>
     
+  </div>
+  </div>
+  <div class=" border-solid border-2 text-black p-10">
+    <h2 class="text-7xl  font-bold p-20">My Projects</h2>
+    <div class="columns-3">
+      <div class="group border-solid border-2 pt-20 text-black px-10 text-center hover:bg-darkblue ease-in duration-200 hover:shadow-md hover:shadow-darkblue">
+        <p class=" text-xl ease-in duration-100 group-hover:text-white group-hover:font-bold">Pleiades Projectworks</p>
+        <b class="ease duration-100 group-hover:font-normal group-hover:text-white">Vue.js / Bootstrap 5 website for web development company</b>
+        <img class="pt-10" src="../assets/img/pleiades.png" alt="">
+      </div>
+      <div class="group border-solid border-2 pt-20 text-black px-10 text-center hover:bg-orange ease-in duration-200 hover:shadow-md hover:shadow-orange">
+        <p class="text-xl ease-in duration-100 group-hover:text-white group-hover:font-bold">Debbie Dye TC Services</p>
+        <b class="ease duration-100 group-hover:font-normal group-hover:text-white">Vue.js / Bootstrap 5 website for transaction coordination buisness</b>
+        <img class="pt-10" src="../assets/img/debdye.png" alt="">
+      </div>
+      <div class=" group border-solid border-2 pt-20 text-black px-10 text-center hover:bg-gray-dark ease-in duration-200 hover:shadow-md hover:shadow-gray-dark">
+        <p class="text-xl ease-in duration-100 group-hover:text-white group-hover:font-bold">Github User Page Clone</p>
+        <b class="ease duration-100 group-hover:font-normal group-hover:text-white">My previous portfolio, made using Vue.js and Bootstrap</b>
+        <img class="pt-10" src="../assets/img/portfolio.png" alt="">
+      </div>
+    </div>
   </div>
 
   
--- my-project/tailwind.config.cjs
+++ my-project/tailwind.config.cjs
@@ -10,14 +10,16 @@ module.exports = {
       'blue': '#1fb6ff',
       'purple': '#7e5bef',
       'pink': '#ff49db',
-      'orange': '#ff7849',
+      'orange': '#DF654B',
       'green': '#13ce66',
       'yellow': '#ffc82c',
-      'gray-dark': '#273444',
+      'gray-dark': '#151A1F',
       'gray': '#8492a6',
       'gray-light': '#d3dce6',
       'black': '#000000',
-      'white': '#ffffff'
+      'white': '#ffffff',
+      'darkblue': "#142845"
+
     },
   },
   plugins: [],
